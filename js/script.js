7c4d97a2820a33d5bace60b1899f894d3dc4638c
Jeremy <jeremy.teurterie@gmail.com>
2023-07-03 15:16:38 +0700

    :sparkles: Page router & active link

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const global = {
+  currentPage: window.location.pathname,
+};
+
+// HighLight active link
+function highlightActiveLink() {
+  const links = document.querySelectorAll('.nav-link');
+  links.forEach((link) => {
+    if (link.getAttribute('href') === global.currentPage) {
+      link.classList.add('active');
+    }
+  });
+}
+
+// Init App
+function init() {
+  switch (global.currentPage) {
+    case '/':
+    case '/index.html':
+      console.log('Home');
+      break;
+    case '/show.html':
+      console.log('Shows');
+      break;
+    case '/movie-details.html':
+      console.log('Movies Details');
+      break;
+    case '/tv-details.html':
+      console.log('TV Details');
+      break;
+    case '/search.html':
+      console.log('Search');
+      break;
+  }
+
+  highlightActiveLink();
+}
+
+document.addEventListener('DOMContentLoaded', init);
